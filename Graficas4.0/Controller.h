145d94ac62861db720e117b87c0f9658e90eac8d
unknown <jose.espinoza.lopez@MTYMT15065WYG.dir.svc.accenture.com>
2015-08-05 20:25:44 -0500

    start control routines based on the ones found on opengl tutorials site

---
+++ Graficas4.0/Controller.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<gl\glew.h>
+#include<GLFW\glfw3.h>
+#include<glm\glm.hpp>
+
+using namespace glm;
+
+class controller{
+public:
+	void getInput(GLFWwindow*,mat4*, mat4*, mat4*);
+private:
+	
+
+
+};
